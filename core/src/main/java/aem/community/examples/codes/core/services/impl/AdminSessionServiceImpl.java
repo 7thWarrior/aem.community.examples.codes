aa91b3d19ede1d5a0ff6353baaba9a819e7907fd
Sundaram Farroda <sa.farroda@gmail.com>
2017-11-09 23:55:26 +0530

    Update admin services
    Update admin services

--- core/src/main/java/aem/community/examples/codes/core/services/impl/AdminSessionServiceImpl.java
+++ core/src/main/java/aem/community/examples/codes/core/services/impl/AdminSessionServiceImpl.java
@@ -3,6 +3,8 @@ package aem.community.examples.codes.core.services.impl;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.jcr.Session;
+
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
@@ -19,6 +21,8 @@ public class AdminSessionServiceImpl implements AdminSessionService{
 	@Reference
 	ResourceResolverFactory resourceResolverFactory;
 	
+	public Session session;
+	
 	@Override
 	public ResourceResolver getAdministrativeResourceResolver() {
 		ResourceResolver resourceResolver=null;		
@@ -30,5 +34,19 @@ public class AdminSessionServiceImpl implements AdminSessionService{
 
 		}			
 		return resourceResolver;
+	}
+	
+	@Override
+	public Session getAdminSession() {
+		ResourceResolver resourceResolver=null;		
+		try {
+			Map<String,Object> paramMap=new HashMap<String,Object>();
+			paramMap.put(ResourceResolverFactory.SUBSERVICE, "Admin-Session-SubService");
+			resourceResolver=resourceResolverFactory.getServiceResourceResolver(paramMap);
+			session=resourceResolver.adaptTo(Session.class);
+		} catch (LoginException e) {
+
+		}			
+		return session;
 	}
 }
