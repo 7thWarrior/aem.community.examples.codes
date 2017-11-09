e4f9ef586ecb444d1075cceb6db6d51a697e4eba
Sundaram Farroda <sa.farroda@gmail.com>
2017-11-09 23:56:11 +0530

    Update
    Update

--- core/src/main/java/aem/community/examples/codes/core/services/AdminSessionService.java
+++ core/src/main/java/aem/community/examples/codes/core/services/AdminSessionService.java
@@ -1,9 +1,12 @@
 package aem.community.examples.codes.core.services;
 
 import org.apache.sling.api.resource.ResourceResolver;
+import javax.jcr.Session;
 
 public interface AdminSessionService {
 	
 	public abstract ResourceResolver getAdministrativeResourceResolver();
+
+	public abstract Session getAdminSession();
 
 }
